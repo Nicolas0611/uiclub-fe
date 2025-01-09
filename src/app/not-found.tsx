33774a87a401e1fe85649a135793044392c69d11
Nicolas Sierra <nico.sierra13@gmail.com>
2025-01-08 20:18:19 -0500

    Add not found page style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,15 @@
+import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <ExclamationCircleIcon className="size-40 text-primary" />
+      <h1 className="text-6xl">Not Found</h1>
       <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <Link className="text-primary" href="/">
+        Return Home
+      </Link>
     </div>
   );
 }
