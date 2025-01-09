54194c0f96e7c86e03875e10b88148398d242fd5
Nicolas Sierra <nico.sierra13@gmail.com>
2025-01-08 21:40:41 -0500

    Add page ProfileContainer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import ProfileContainer from "@/components/custom/ProfileContainer";
+
 export default function Home() {
-  return <p>Hjujkasks hhj jhakasku h asiol no manches a</p>;
+  return (
+    <section className="py-10 px-20">
+      <div className="flex gap-4">
+        <ProfileContainer />
+        <div className="bg-slate-600 w-1/2">hola</div>
+      </div>
+    </section>
+  );
 }
