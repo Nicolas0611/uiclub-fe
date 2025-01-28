b901b98d24184fa40036acab1b9efe00c4e123d9
Nicolás Sierra <nico.sierra13@gmail.com>
2025-01-28 00:28:30 -0500

    Delete unnecesary middleware

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  webpackDevMiddleware: (config) => {
-    // Ensure hot reloading works in Docker
-    config.watchOptions = {
-      poll: 1000, // Check for changes every second
-      aggregateTimeout: 300, // Delay rebuild after the first change
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
