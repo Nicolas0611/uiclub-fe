ebf13d103ea873d07656be92d6d5037fbf2d779a
Nicolás Sierra <nico.sierra13@gmail.com>
2025-01-28 00:28:48 -0500

    add herui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { heroui } from "@heroui/react";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -5,16 +6,16 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: "var(--primary)",
         primary_dark: "var(--primary-dark)",
-        background: "var(--background)",
-        foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 } satisfies Config;
