087cd0e438dfb0dc10e745df857e5b36658442f0
Nicolás Sierra <nico.sierra13@gmail.com>
2025-02-17 21:07:10 -0500

    add button images

--- src/design-systems/components/ComponentsGrid/ComponentsGrid.tsx
+++ src/design-systems/components/ComponentsGrid/ComponentsGrid.tsx
@@ -1,13 +1,13 @@
 "use client";
+
 import { Component } from "@/interfaces/design-system-interface";
 import { Card, CardBody, CardFooter, Image } from "@heroui/react";
-
 interface ComponentsProps {
   components: Component[];
 }
 const ComponentsGrid = ({ components }: ComponentsProps) => {
   return (
-    <div className="grid grid-cols-4 gap-7 py-4">
+    <div className="grid grid-cols-4 gap-7 py-6">
       {components.map((component, index) => (
         <Card
           key={index}
@@ -20,8 +20,9 @@ const ComponentsGrid = ({ components }: ComponentsProps) => {
             <Image
               alt={`${component.name}_img`}
               className="w-full object-cover h-[140px]"
-              src="https://miro.medium.com/v2/resize:fit:1400/1*M3DW73fGv1nV3CuabO8yiQ.png"
+              src="/assets/Button.png"
               width="100%"
+              height="100%"
             />
           </CardBody>
           <CardFooter className="text-small justify-between">
