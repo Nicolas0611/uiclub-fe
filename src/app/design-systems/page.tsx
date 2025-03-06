4700f43d5debafeb6681df5974e62ceeb1c9bf1e
Nicolás Sierra <nico.sierra13@gmail.com>
2025-03-05 21:21:18 -0500

    Add handleSearch component for input search

--- src/app/design-systems/page.tsx
+++ src/app/design-systems/page.tsx
@@ -7,7 +7,13 @@ export const metadata: Metadata = {
   description: "Design System Descripton for metadata",
 };
 
-async function DesignSystemsPage() {
+async function DesignSystemsPage({
+  searchParams,
+}: {
+  searchParams: { search: string };
+}) {
+  const { search } = await searchParams;
+
   const content = {
     preTitle: "2024 · Design Systems and UI Libraries",
     title: "List of the top 20 Benchmark Systems",
@@ -15,7 +21,7 @@ async function DesignSystemsPage() {
       " This is the list of the most popular Design Systems and UI libraries, used as the leading Systems Benchmarks by thousands of companies, startups, designers, and developers around the world. This list is the result of exhaustive research, which included a survey of hundreds of designers and developers working at leading companies, such as Google, Atlassian, Airbnb, and IBM, among others.",
   };
 
-  const designSystems = await fetchDesignSystems();
+  const designSystems = await fetchDesignSystems(search);
   return (
     <section className="container mx-auto px-4 py-10">
       <div className="flex flex-col gap-5">
--- src/components/shared/Tabs/SwitchTabs.tsx
+++ src/components/shared/Tabs/SwitchTabs.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Divider, Tab, Tabs } from "@heroui/react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { Divider, Input, Tab, Tabs } from "@heroui/react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { ReactNode, useState } from "react";
 
 interface TabProps {
@@ -11,30 +13,62 @@ interface TabProps {
 
 const SwitchTab = ({ tabs, defaultKey, title }: TabProps) => {
   const [selected, setSelected] = useState<string>(defaultKey);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const search = searchParams.get("search") ?? "";
 
   const handleSelect = (key: string) => {
     setSelected(key);
   };
-
+  //! add debounced handleSearch
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("search", term);
+    } else {
+      params.delete("search");
+    }
+    router.replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <div className="flex w-full flex-col">
       {title && <h3 className="text-lg pb-3">{title}</h3>}
       <Divider />
-      <Tabs
-        size="sm"
-        aria-label="Options"
-        color="primary"
-        variant="bordered"
-        selectedKey={selected}
-        onSelectionChange={(key) => handleSelect(key as string)}
-        className="flex justify-end pt-3"
-      >
-        {tabs.map((tab) => (
-          <Tab key={tab.key} title={tab.title}>
-            {tab.children}
-          </Tab>
-        ))}
-      </Tabs>
+
+      <div className="flex flex-wrap w-full pt-5">
+        <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[20rem] h-10 mr-auto pb-1",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper: "h-full font-normal text-default-500",
+          }}
+          value={search}
+          placeholder="Type to search..."
+          size="sm"
+          startContent={<MagnifyingGlassIcon className="size-5" />}
+          type="search"
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+        />
+        <Tabs
+          size="sm"
+          aria-label="Options"
+          color="primary"
+          variant="bordered"
+          selectedKey={selected}
+          onSelectionChange={(key) => handleSelect(key as string)}
+          className="ml-auto pb-1"
+        >
+          {tabs.map((tab) => (
+            <Tab className="w-full" key={tab.key} title={tab.title}>
+              {tab.children}
+            </Tab>
+          ))}
+        </Tabs>
+      </div>
     </div>
   );
 };
