dd32dce23455186e2f884ace607e774aca01e0df
Nicolás Sierra <nico.sierra13@gmail.com>
2025-03-09 14:11:14 -0500

    add some secrets

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export const https = axios.create({
-  baseURL: "http://backend:8000/api/", // Replace with your API base URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Replace with your API base URL
   timeout: 10000, // Optional: Timeout for requests
   headers: {
     "Content-Type": "application/json",
   },
 });
-  
