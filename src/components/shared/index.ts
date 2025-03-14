ee2de895c27c101baf60df61cf2e5383a2b39ef5
Nicolás Sierra <nico.sierra13@gmail.com>
2025-03-13 20:32:40 -0500

    Add header new component

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,4 @@
 export { default as Breadcrumb } from "./Breadcrumbs/Breadcrumb";
+export { default as Header } from "./Navbar/Header";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as SwitchTabs } from "./Tabs/SwitchTabs";
