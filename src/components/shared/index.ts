eb9d071e15d03253fdcca1eefa2860259a7b2365
Nicolás Sierra <nico.sierra13@gmail.com>
2025-03-20 22:22:13 -0500

    Add Button and Card to barriel archive

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,5 @@
 export { default as Breadcrumb } from "./Breadcrumbs/Breadcrumb";
+export { default as Button } from "./Button/Button";
+export { Card } from "./Card/Card";
 export { default as Header } from "./Navbar/Header";
 export { default as SwitchTabs } from "./Tabs/SwitchTabs";
