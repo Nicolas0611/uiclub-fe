56fabb048131459b3d460a5cc3392ea24ccd886e
Nicolás Sierra <nico.sierra13@gmail.com>
2025-03-24 20:28:14 -0500

    Add overflow-hidden for the gradient layout header

--- src/app/design-systems/layout.tsx
+++ src/app/design-systems/layout.tsx
@@ -3,5 +3,12 @@ export default function DesignLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="relative">{children}</div>;
+  return (
+    <div className="overflow-hidden">
+      <div className="relative mx-auto max-w-7xl">
+        <div className="z-10 absolute -right-60 h-60 w-[36rem] transform-gpu md:bg-[linear-gradient(115deg,var(--tw-gradient-stops))] from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff] rotate-[-10deg] rounded-full blur-3xl "></div>
+        {children}
+      </div>
+    </div>
+  );
 }
