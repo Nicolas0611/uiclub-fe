cdf8071fb7f62a5a2739b96e1981a5616f2dc9ed
Nicolás Sierra <nico.sierra13@gmail.com>
2025-04-06 20:28:11 -0500

    Add more font weights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,10 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({ weight: ["400", "700"], subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "uiclub",
