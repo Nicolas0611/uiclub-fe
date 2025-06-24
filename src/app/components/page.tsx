ba593474e09105b4f934e0daa65e79f5fba1dfce
Nicolás Sierra <nico.sierra13@gmail.com>
2025-06-23 19:43:45 -0500

    Delete console log

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,4 +1,3 @@
-import { fetchComponentList } from "@/component/actions/component-action";
 import ComponentsGrid from "@/design-systems/components/ComponentsGrid/ComponentsGrid";
 import { Metadata } from "next";
 
@@ -15,8 +14,8 @@ async function ComponentsPage() {
     parragraph:
       "We have compiled all the wisdom and best practices from the top 20 Design Systems and UI libraries in one place. We will continue to add more components week after week.",
   };
-  const components = await fetchComponentList();
-  console.log(components);
+  /*   const components = await fetchComponentList();
+   */
   return (
     <section className="container mx-auto px-4 py-10 ">
       <div className="flex flex-col gap-5">
