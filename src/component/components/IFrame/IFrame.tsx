2e15a3fad9fe7b01ae6c735e2467818b3c8934c4
Nicolás Sierra <nico.sierra13@gmail.com>
2025-07-17 21:56:08 -0500

    Enhance IFrame component styling for better responsiveness

--- src/component/components/IFrame/IFrame.tsx
+++ src/component/components/IFrame/IFrame.tsx
@@ -3,8 +3,14 @@ interface Props {
 }
 const IFrame = ({ link }: Props) => {
   return (
-    <div className="border border-gray-200 rounded-2xl overflow-hidden">
-      <iframe width="100%" height="460" src={link} allowFullScreen></iframe>
+    <div className="w-full border border-gray-200 rounded-2xl overflow-hidden">
+      <iframe
+        className="w-full"
+        width="100%"
+        height="460"
+        src={link}
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
