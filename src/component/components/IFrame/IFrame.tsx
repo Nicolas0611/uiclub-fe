edcefa84b5d8c7f98225b55ad95b980badc8bf06
Nicolás Sierra <nico.sierra13@gmail.com>
2025-07-17 21:13:22 -0500

    Adjust height of IFrame

--- src/component/components/IFrame/IFrame.tsx
+++ src/component/components/IFrame/IFrame.tsx
@@ -4,7 +4,7 @@ interface Props {
 const IFrame = ({ link }: Props) => {
   return (
     <div className="border border-gray-200 rounded-2xl overflow-hidden">
-      <iframe width="100%" height="450" src={link} allowFullScreen></iframe>
+      <iframe width="100%" height="460" src={link} allowFullScreen></iframe>
     </div>
   );
 };
