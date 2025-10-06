8aea64b8d94e5aed1690a5b4d7a389264ca5e04a
Nicolás Sierra <nico.sierra13@gmail.com>
2025-10-05 21:27:28 -0500

    add into seed initial links and company logos

--- src/constants/seed.ts
+++ src/constants/seed.ts
@@ -1628,3 +1628,77 @@ export const initialComponentData = [
     id: 2,
   },
 ];
+
+export const initialLinks = [
+  {
+    state: true,
+    figma: "https://www.figma.com/@brainly",
+    web: "https://design.brainly.com/8adfd5f36/p/10bcb9-pencil",
+    storybook: "https://styleguide.brainly.com/227.0.0/docs/",
+    name: "Brainly",
+  },
+  {
+    state: true,
+    figma: "https://www.figma.com/@atlassian",
+    web: null,
+    storybook: "https://atlassian.design/",
+    name: "Atlassian",
+  },
+  {
+    state: true,
+    figma: "https://www.figma.com/@apple",
+    web: "https://developer.apple.com/design/",
+    storybook: "https://developer.apple.com/design/human-interface-guidelines",
+    name: "Apple",
+  },
+  {
+    state: true,
+    figma: "https://www.figma.com/@cloudscape",
+    web: null,
+    storybook: "https://cloudscape.design/",
+    name: "Amazon",
+  },
+  {
+    state: true,
+    figma: null,
+    web: "https://spectrum.adobe.com/",
+    storybook:
+      "https://storybook.js.org/showcase/adobe-spectrum-web-components",
+    name: "Adobe",
+  },
+  {
+    state: true,
+    figma: null,
+    web: "https://www.activecampaign.design/0e735378a/p/198f2c-activecampaign-design",
+    storybook:
+      "https://storybook.activecampaign.design/?path=/docs/components-accordion--docs",
+    name: "ActiveCampaign",
+  },
+];
+
+export const initialDSImages = [
+  {
+    name: "Amazon",
+    url: "https://res.cloudinary.com/archikola/image/upload/v1759716115/design-system-thumbs/Amazon_nugsoy.png",
+  },
+  {
+    name: "Atlassian",
+    url: "https://res.cloudinary.com/archikola/image/upload/v1759716114/design-system-thumbs/Attlasian_hzdxxj.png",
+  },
+  {
+    name: "Apple",
+    url: "https://res.cloudinary.com/archikola/image/upload/v1759716114/design-system-thumbs/Apple_p4x4zz.png",
+  },
+  {
+    name: "ActiveCampaign",
+    url: "https://res.cloudinary.com/archikola/image/upload/v1759716114/design-system-thumbs/ActiveCapaign_p0y6vz.png",
+  },
+  {
+    name: "Adobe",
+    url: "https://res.cloudinary.com/archikola/image/upload/v1759716114/design-system-thumbs/Adobe_qaln9i.png",
+  },
+  {
+    name: "Brainly",
+    url: "https://res.cloudinary.com/archikola/image/upload/v1759716114/design-system-thumbs/Brainly_xhus3a.png",
+  },
+];
