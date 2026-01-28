c8edf884913a5000b470c4f74ad2bcccd658bed5
Nicolás Sierra <nico.sierra13@gmail.com>
2026-01-27 23:19:01 -0500

    Add User interface to user-interface.ts
    - Introduced a new `User` interface defining user properties including `id`, `name`, `email`, `emailVerified`, `password`, `role`, and `image`.
    - Added a `Role` type to specify user roles as either `admin` or `user`.

---
+++ src/interfaces/user-interface.ts
@@ -0,0 +1,11 @@
+type Role = "admin" | "user";
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  emailVerified: Date;
+  password: string;
+  role: Role;
+  image: string;
+}
