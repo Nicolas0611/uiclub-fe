1a780999d6200491120e41cc84cdeb187ab124d2
Nicolás Sierra <nico.sierra13@gmail.com>
2026-01-30 23:23:15 -0500

    feat: enhance Sidebar component with role-based menu items and logout functionality
    - Integrated role-based filtering for menu items in the Sidebar based on user roles (admin and all).
    - Added logout functionality to the Sidebar, allowing users to log out and redirect to the home page.
    - Updated User component to include the new onLogout prop for handling logout actions.

--- src/components/shared/Sidebar/Siderbar.data.tsx
+++ src/components/shared/Sidebar/Siderbar.data.tsx
@@ -8,30 +8,40 @@ import {
 } from "@heroicons/react/24/outline";
 
 export const menuItems = [
-  { key: "home", label: "Home", icon: <HomeIcon className="size-5" /> },
+  {
+    key: "home",
+    label: "Home",
+    icon: <HomeIcon className="size-5" />,
+    role: "all",
+  },
   {
     key: "design-systems",
     label: "Design Systems",
     icon: <PencilIcon className="size-5" />,
+    role: "all",
   },
   {
     key: "components",
     label: "Components",
     icon: <PuzzlePieceIcon className="size-5" />,
+    role: "all",
   },
   {
     key: "links",
     label: "Links",
     icon: <LinkIcon className="size-5" />,
+    role: "all",
   },
   {
     key: "users",
     label: "Users",
     icon: <UsersIcon className="size-5" />,
+    role: "admin",
   },
   {
     key: "companies",
     label: "Companies",
     icon: <BuildingStorefrontIcon className="size-5" />,
+    role: "all",
   },
 ];
