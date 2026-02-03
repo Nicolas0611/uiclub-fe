e55e23e4c654582a6dc0f0f69dd3247bbf5ab0b4
Nicolás Sierra <nico.sierra13@gmail.com>
2026-02-02 23:43:49 -0500

    feat: update Prisma adapter interface with new design system and company types
    - Added `IDesignSystemFindMany` and `IDesignSystemFindFirst` types for design system queries.
    - Introduced `ICompanyFindMany` and `ICompanyFindFirst` types for company data retrieval.
    - Organized type definitions into sections for better clarity and maintainability.

--- src/interfaces/adapters/prisma-adapter-interface.ts
+++ src/interfaces/adapters/prisma-adapter-interface.ts
@@ -1,12 +1,22 @@
 import { Prisma } from "@prisma/client";
 
+/* DESIGN SYSTEM */
 export type IDesignSystemFindMany = Prisma.DesignSystemFindManyArgs;
-export type IComponentTypeFindMany = Prisma.ComponentTypeFindFirstArgs;
 export type IDesignSystemFindFirst = Prisma.DesignSystemFindFirstArgs;
+
+/* COMPONENT TYPE */
+export type IComponentTypeFindMany = Prisma.ComponentTypeFindFirstArgs;
 export type IComponentFindMany = Prisma.ComponentFindManyArgs;
+
+/* USER */
 export type IUserFindUnique = Prisma.UserFindUniqueArgs;
-export type IModelNames = Prisma.ModelName;
+
+/* COMPANY */
 export type ICompanyFindMany = Prisma.CompanyFindManyArgs;
+export type ICompanyFindFirst = Prisma.CompanyFindFirstArgs;
+
+/* MODEL NAMES */
+export type IModelNames = Prisma.ModelName;
 
 export interface HttpsReq<RES_T, OPT_T> {
   findMany(options?: OPT_T): Promise<RES_T>;
