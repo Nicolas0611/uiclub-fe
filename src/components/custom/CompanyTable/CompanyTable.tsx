e5fca3d2cd434bc5d2f262fa48b2b9eabff4d93b
Nicolás Sierra <nico.sierra13@gmail.com>
2026-02-02 23:43:57 -0500

    refactor: clean up CompanyTable component by removing unused types and sample data
    - Removed the `IconSvgProps` type definition as it was not utilized.
    - Deleted the `users` array containing sample data to streamline the component.
    - Updated the edit link to point to the company detail page directly.

--- src/components/custom/CompanyTable/CompanyTable.tsx
+++ src/components/custom/CompanyTable/CompanyTable.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { ChipProps } from "@heroui/react";
-import type { SVGProps } from "react";
 
 import { Company } from "@/interfaces/company-interface";
 import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
@@ -19,68 +18,11 @@ import {
 } from "@heroui/react";
 import React, { useCallback } from "react";
 
-export type IconSvgProps = SVGProps<SVGSVGElement> & {
-  size?: number;
-};
-
 export const columns = [
   { name: "Name", uid: "name" },
   { name: "Design System", uid: "designSystem" },
   { name: "Status", uid: "status" },
   { name: "Actions", uid: "actions" },
-];
-
-export const users = [
-  {
-    id: 1,
-    name: "Tony Reichert",
-    role: "CEO",
-    team: "Management",
-    status: "active",
-    age: "29",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    email: "tony.reichert@example.com",
-  },
-  {
-    id: 2,
-    name: "Zoey Lang",
-    role: "Technical Lead",
-    team: "Development",
-    status: "paused",
-    age: "25",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-    email: "zoey.lang@example.com",
-  },
-  {
-    id: 3,
-    name: "Jane Fisher",
-    role: "Senior Developer",
-    team: "Development",
-    status: "active",
-    age: "22",
-    avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-    email: "jane.fisher@example.com",
-  },
-  {
-    id: 4,
-    name: "William Howard",
-    role: "Community Manager",
-    team: "Marketing",
-    status: "vacation",
-    age: "28",
-    avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
-    email: "william.howard@example.com",
-  },
-  {
-    id: 5,
-    name: "Kristen Copper",
-    role: "Sales Manager",
-    team: "Sales",
-    status: "active",
-    age: "24",
-    avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
-    email: "kristen.cooper@example.com",
-  },
 ];
 
 const statusColorMap: Record<string, ChipProps["color"]> = {
@@ -136,7 +78,7 @@ const CompanyTable = ({ companies = [] }: { companies: Company[] }) => {
         return (
           <div className="flex items-center gap-2 justify-center">
             <Tooltip content="Edit">
-              <Link href={`/dashboard/companies/${company.id}/edit`}>
+              <Link href={`/dashboard/companies/${company.id}/`}>
                 <PencilIcon className="size-5 text-default-400" />
               </Link>
             </Tooltip>
