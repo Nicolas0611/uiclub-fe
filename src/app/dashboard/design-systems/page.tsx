6ea51b0d0b0aef893a5d72e0bbb28cb801088f1e
Nicolás Sierra <nico.sierra13@gmail.com>
2026-02-04 21:54:58 -0500

    feat: enhance CompanyDetailPage and DesignSystemPage with new components
    - Integrated `FormWrapper` in `CompanyDetailPage` for improved layout and consistency.
    - Updated `DesignSystemPage` to fetch design systems asynchronously and display them using `AdminGridCard` for better UI representation.

--- src/app/dashboard/design-systems/page.tsx
+++ src/app/dashboard/design-systems/page.tsx
@@ -1,5 +1,9 @@
-const DesignSystemPage = () => {
-  return <div>DesignSystemPage</div>;
+import { fetchDesignSystems } from "@/actions/design-system/design-actions";
+import AdminGridCard from "./ui/AdminGridCard";
+
+const DesignSystemPage = async () => {
+  const designSystems = await fetchDesignSystems();
+  return <AdminGridCard designSystems={designSystems} />;
 };
 
 export default DesignSystemPage;
