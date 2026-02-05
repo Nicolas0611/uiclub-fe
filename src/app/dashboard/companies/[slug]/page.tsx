c8a3789ac4c6e30c88e4de6d16488dfa76d3c279
Nicolas Sierra <61467414+Nicolas0611@users.noreply.github.com>
2026-02-04 22:13:25 -0500

    feat: Create Design System Admin Page (#67)
    * feat: add overflow-y-auto to children container in DashboardLayout for improved scrolling
    
    * feat: enhance CompanyDetailPage and DesignSystemPage with new components
    
    - Integrated `FormWrapper` in `CompanyDetailPage` for improved layout and consistency.
    - Updated `DesignSystemPage` to fetch design systems asynchronously and display them using `AdminGridCard` for better UI representation.
    
    * feat: add DesignDetailPage and DesignSystemForm components for design system management
    
    - Introduced `DesignDetailPage` to fetch and display design system details, integrating `FormWrapper` for layout consistency.
    - Created `DesignSystemForm` component with form handling using `react-hook-form` for managing design system data, including fields for name, descriptions, and company associations.
    
    * feat: create AdminGridCard component for displaying design systems in a grid layout
    
    - Introduced `AdminGridCard` component to showcase design systems with a responsive grid layout.
    - Implemented empty state handling for scenarios with no design systems available.
    - Enhanced existing `GridCards` component to support additional header and chip functionalities for design system representation.
    - Refactored `Card` component to accept a customizable header, improving flexibility for different use cases.
    
    * feat: add FormWrapper component for consistent form layout
    
    - Introduced `FormWrapper` component to standardize the layout of forms with an icon and title.
    - Enhanced visual consistency across forms by providing a reusable wrapper for form elements.

--- src/app/dashboard/companies/[slug]/page.tsx
+++ src/app/dashboard/companies/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getCompanyById } from "@/actions/company/get-company-by-id";
+import FormWrapper from "@/components/shared/FormWrapper/FormWrapper";
 import { Company } from "@/interfaces/company-interface";
 import { BuildingStorefrontIcon } from "@heroicons/react/24/outline";
 import { redirect } from "next/navigation";
@@ -22,13 +23,12 @@ const CompanyDetailPage = async ({
   }
 
   return (
-    <div className="container mx-auto bg-white p-4 border border-gray-200 rounded-2xl flex flex-col gap-4 w-full max-w-3xl">
-      <div className="flex items-center gap-2 border-b border-gray-200 pb-4">
-        <BuildingStorefrontIcon className="size-5 text-gray-500" />
-        <h1 className="text-sm text-gray-500">Nueva empresa</h1>
-      </div>
+    <FormWrapper
+      icon={<BuildingStorefrontIcon className="size-5 text-gray-500" />}
+      title="Nueva empresa"
+    >
       <CompanyForm company={company} />
-    </div>
+    </FormWrapper>
   );
 };
 
