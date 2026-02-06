23c6039c757c32c38ca7b3bf3fbab8311386ed6e
Nicolás Sierra <nico.sierra13@gmail.com>
2026-02-05 23:12:44 -0500

    feat: add getCompanyImages action for retrieving company images
    - Introduced `getCompanyImages` function to fetch multiple company images using the PrismaAdapter.
    - Implemented error handling and response messaging for scenarios where images are found or not found, enhancing user feedback.

---
+++ src/actions/company/get-company-image.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { PrismaAdapter } from "@/adapters/PrismaAdapter";
+import { ICompanyImageFindMany } from "@/interfaces/adapters/prisma-adapter-interface";
+import { CompanyImage } from "@/interfaces/design-system-interface";
+
+export const getCompanyImages = async () => {
+  try {
+    const req = new PrismaAdapter<CompanyImage[], ICompanyImageFindMany>(
+      "CompanyImage",
+    );
+    const response = await req.findMany();
+    if (!response)
+      return { images: [], ok: true, message: "No companies images found" };
+
+    return { images: response, ok: true, message: "Companies images found" };
+  } catch (error) {
+    console.error(error);
+    return {
+      images: [],
+      ok: false,
+      message: `Error getting company images ${error}`,
+    };
+  }
+};
