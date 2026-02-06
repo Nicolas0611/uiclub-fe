644df4a9b50f48e3bfdb539a97da1813a613d529
Nicolás Sierra <nico.sierra13@gmail.com>
2026-02-05 23:12:51 -0500

    feat: add createUpdateDesignSystem action for managing design systems
    - Introduced `createUpdateDesignSystem` function to handle both creation and updating of design systems using Prisma.
    - Implemented validation with Zod to ensure data integrity before processing.
    - Added error handling and response messaging for successful and failed operations, enhancing user feedback.
    - Integrated path revalidation to update the dashboard after changes.

---
+++ src/actions/design-system/create-update-design.ts
@@ -0,0 +1,78 @@
+"use server";
+
+import { DesignSystemFormValues } from "@/app/dashboard/design-systems/ui/DesignSystemForm";
+import { prisma } from "@/lib/prisma";
+import { DesignSystem } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import z from "zod";
+
+const designSystemSchema = z.object({
+  id: z.string().optional(),
+  name: z.string().min(3).max(400),
+  state: z.boolean(),
+  popularity: z.enum(["HIGH", "MEDIUM", "LOW"]),
+  isUpdated: z.boolean(),
+  slug: z.string().min(3).max(255),
+  isNew: z.boolean(),
+  companyId: z.string(),
+  companyImageId: z.string(),
+  shortDescription: z.string().min(3),
+  largeDescription: z.string().min(3),
+});
+
+export const createUpdateDesignSystem = async (
+  designSystem: DesignSystemFormValues,
+) => {
+  const designSystemParsed = designSystemSchema.safeParse(designSystem);
+
+  console.log({ designSystem });
+  if (!designSystemParsed.success) {
+    return {
+      message: designSystemParsed.error.message,
+      ok: false,
+    };
+  }
+
+  const designSystemData = designSystemParsed.data;
+  const { id, companyId, companyImageId, ...rest } = designSystemData;
+
+  try {
+    const prismaTx = await prisma.$transaction(async (tx) => {
+      let designSystem: DesignSystem;
+
+      if (id) {
+        // Actualizar design system
+        designSystem = await tx.designSystem.update({
+          where: { id: id },
+          data: {
+            ...rest,
+            companyId: Number(companyId),
+            companyImageId: companyImageId,
+          },
+        });
+        console.log({ updatedDesignSystem: designSystem });
+      } else {
+        //Crear empresa
+        designSystem = await tx.designSystem.create({
+          data: {
+            ...rest,
+            companyId: Number(companyId),
+            companyImageId: companyImageId,
+          },
+        });
+        console.log({ createdDesignSystem: designSystem });
+      }
+    });
+
+    revalidatePath(`/dashboard/design-systems`);
+
+    return {
+      ok: true,
+      product: prismaTx,
+      message: "Compañía creada/actualizado exitosamente",
+    };
+  } catch (error) {
+    console.error(error);
+    return { ok: false, message: "Error al crear/actualizar la compañía" };
+  }
+};
