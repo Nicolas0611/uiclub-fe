7780a64e060b73f36b287c9a7be4698266060a04
Nicolás Sierra <nico.sierra13@gmail.com>
2026-02-08 22:31:49 -0500

    feat: Add get links action

---
+++ src/actions/links/get-links.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { PrismaAdapter } from "@/adapters/PrismaAdapter";
+import { ILinkFindMany } from "@/interfaces/adapters/prisma-adapter-interface";
+import { Link } from "@/interfaces/design-system-interface";
+
+export const getLinks = async () => {
+  try {
+    const req = new PrismaAdapter<Link[], ILinkFindMany>("Link");
+    const response = await req.findMany({
+      include: {
+        designSystem: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    if (!response) return { links: [], ok: false, message: "No links found" };
+
+    return { links: response, ok: true, message: "Links found" };
+  } catch (error) {
+    console.error(error);
+    return {
+      links: [],
+      ok: false,
+      message: `Error fetching links ${error}`,
+    };
+  }
+};
