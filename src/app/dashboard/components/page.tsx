a2b5fd30e60fed3b6e760eec3c6c5c6e75dfb62d
Nicolás Sierra <nico.sierra13@gmail.com>
2026-02-10 19:18:14 -0500

    feat: Add component module to the dashboard page and add Component Table

--- src/app/dashboard/components/page.tsx
+++ src/app/dashboard/components/page.tsx
@@ -1,5 +1,20 @@
-const ComponentsPage = () => {
-  return <div>ComponentsPage</div>;
+import { getComponents } from "@/actions/component/get-components";
+import { ComponentTable } from "./ui/ComponentTable/ComponentTable";
+
+interface IComponentsPage {
+  searchParams: Promise<{ page: number; take: number }>;
+}
+const ComponentsPage = async ({ searchParams }: IComponentsPage) => {
+  const { page } = await searchParams;
+
+  const { components, ok, message, totalPages } = await getComponents({ page });
+
+  if (!ok) return <p>{message}</p>;
+  return (
+    <div className="flex flex-col gap-4">
+      <ComponentTable components={components} totalPages={totalPages} />
+    </div>
+  );
 };
 
 export default ComponentsPage;
---
+++ src/app/dashboard/components/ui/ComponentTable/ComponentTable.tsx
@@ -0,0 +1,172 @@
+"use client";
+
+import type { Component } from "@/interfaces/design-system-interface";
+import { PencilIcon } from "@heroicons/react/24/outline";
+import {
+  Chip,
+  ChipProps,
+  Link,
+  Pagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  Tooltip,
+  User,
+} from "@heroui/react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+
+interface IComponentTable {
+  components: Component[];
+  totalPages: number;
+}
+
+export const columns = [
+  { name: "Component", uid: "component" },
+  { name: "Status", uid: "status" },
+  { name: "Type", uid: "type" },
+  { name: "Company", uid: "company" },
+  { name: "Design System", uid: "designSystem" },
+  { name: "Website", uid: "website" },
+  { name: "Actions", uid: "actions" },
+];
+const statusColorMap: Record<string, ChipProps["color"]> = {
+  active: "success",
+  paused: "danger",
+};
+
+export const ComponentTable = ({ components, totalPages }: IComponentTable) => {
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const createPageUrl = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (+pageNumber <= 0) {
+      return `${pathName}`;
+    }
+
+    if (+pageNumber > totalPages) {
+      return `${pathName}?${params.toString()}`;
+    }
+
+    params.set("page", pageNumber.toString());
+    return `${pathName}?${params.toString()}`;
+  };
+
+  const renderCell = useCallback(
+    (component: Component, columnKey: React.Key) => {
+      switch (columnKey) {
+        case "component":
+          return (
+            <User
+              name={component.name}
+              avatarProps={{
+                radius: "lg",
+                src: `/assets/${component.name}.png`,
+              }}
+            >
+              {component.name}
+            </User>
+          );
+        case "company":
+          return (
+            <p className="text-bold text-sm capitalize">
+              {component?.designSystem?.company?.name}
+            </p>
+          );
+        case "designSystem":
+          return (
+            <div className="flex flex-col">
+              {component?.designSystem?.name ? (
+                <p className="text-bold text-sm capitalize">
+                  {component?.designSystem?.name}
+                </p>
+              ) : (
+                <Chip
+                  className="capitalize"
+                  color="warning"
+                  size="sm"
+                  variant="flat"
+                >
+                  No associated DS
+                </Chip>
+              )}
+            </div>
+          );
+        case "status":
+          return (
+            <Chip
+              className="capitalize"
+              color={statusColorMap[component.state ? "active" : "paused"]}
+              size="sm"
+              variant="flat"
+            >
+              {component.state ? "Active" : "Inactive"}
+            </Chip>
+          );
+        case "website":
+          return (
+            <Link isExternal href={component.link} size="sm">
+              Link
+            </Link>
+          );
+        case "type":
+          return (
+            <Chip size="sm" color="success" variant="dot">
+              {component?.type}
+            </Chip>
+          );
+        case "actions":
+          return (
+            <div className="flex items-center gap-2 justify-center">
+              <Tooltip content="Edit">
+                <Link href={`/dashboard/components/${component.id}/`}>
+                  <PencilIcon className="size-5 text-default-400" />
+                </Link>
+              </Tooltip>
+            </div>
+          );
+      }
+    },
+    [],
+  );
+  return (
+    <Table
+      aria-label="Components Table"
+      shadow="none"
+      className="border border-gray-200 rounded-lg"
+      bottomContent={
+        <div className="flex w-full justify-end">
+          <Pagination
+            isCompact
+            showControls
+            showShadow
+            color="primary"
+            total={totalPages}
+            onChange={(page) => router.push(createPageUrl(page))}
+          />
+        </div>
+      }
+    >
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.uid}>{column.name}</TableColumn>}
+      </TableHeader>
+      <TableBody items={components}>
+        {(component) => (
+          <TableRow key={component.id}>
+            {(columnKey) => (
+              <TableCell key={columnKey}>
+                {renderCell(component, columnKey)}
+              </TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
