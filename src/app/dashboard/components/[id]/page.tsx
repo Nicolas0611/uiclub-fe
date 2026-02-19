fcae2ec482fd49631598196b3366a0db952b4291
Nicolás Sierra <nico.sierra13@gmail.com>
2026-02-18 19:42:24 -0500

    feat: Implement getComponentById action and enhance component detail page
    - Added a new action to fetch component details by ID, including related images and design system information.
    - Updated the ComponentDetailPage to utilize the new action for displaying individual component details.
    - Introduced a button for adding new components in the ComponentsPage for improved user experience.

--- src/app/dashboard/components/[id]/page.tsx
+++ src/app/dashboard/components/[id]/page.tsx
@@ -1,5 +1,36 @@
-const ComponentDetailPage = () => {
-  return <div>ComponentDetailPage</div>;
+import { getComponentById } from "@/actions/component/get-component-by-id";
+import FormWrapper from "@/components/shared/FormWrapper/FormWrapper";
+import { Component } from "@/interfaces/design-system-interface";
+import { PencilIcon } from "@heroicons/react/24/outline";
+import { redirect } from "next/navigation";
+
+const ComponentDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  let component: Component = {} as Component;
+
+  if (id !== "new") {
+    const componentResponse = await getComponentById(id);
+    if (componentResponse.ok) {
+      component = componentResponse.component!;
+    }
+  }
+  if (!component && id !== "new") {
+    redirect("/dashboard/components");
+  }
+  console.log(component);
+
+  return (
+    <FormWrapper
+      icon={<PencilIcon className="size-5 text-gray-500" />}
+      title="Component"
+    >
+      <div>ComponentDetailPage</div>
+    </FormWrapper>
+  );
 };
 
 export default ComponentDetailPage;
