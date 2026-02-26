96c923bf7104b7e48ea36fd12301588f3199fa35
Nicolás Sierra <nico.sierra13@gmail.com>
2026-02-25 19:39:31 -0500

    feat: Implement deleteUser action for user management
    - Added a new deleteUser function to handle user deletion with admin role verification.
    - Integrated PrismaAdapter for database operations and included error handling for deletion failures.
    - Implemented revalidation of the user dashboard path upon successful deletion.

---
+++ src/actions/users/delete-user.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { PrismaAdapter } from "@/adapters/PrismaAdapter";
+import { auth } from "@/auth.config";
+import { IUserDelete } from "@/interfaces/adapters/prisma-adapter-interface";
+import { User } from "@/interfaces/user-interface";
+import { revalidatePath } from "next/cache";
+
+export const deleteUser = async (id: string) => {
+  const session = await auth();
+  const isAdmin = session?.user?.role === "admin";
+
+  if (!isAdmin) {
+    return {
+      ok: false,
+      message: "No tienes permisos para acceder a esta página",
+    };
+  }
+
+  try {
+    const req = new PrismaAdapter<User[], IUserDelete>("User");
+    const users = await req.delete({
+      where: {
+        id: id,
+      },
+    });
+    if (!users) {
+      return {
+        ok: false,
+        message: "No se pudo eliminar el usuario",
+      };
+    }
+    revalidatePath("/dashboard/users");
+    return {
+      ok: true,
+      message: "Usuario eliminado correctamente",
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: "No se pudo obtener los usuarios",
+    };
+  }
+};
