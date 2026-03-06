86edbf46ce4ac4b0f322984e0724fe90f98dde1c
Nicolás Sierra <nico.sierra13@gmail.com>
2026-03-05 21:56:53 -0500

    feat: Enhance FigmaForm to include company and component type selection
    - Updated FigmaForm component to accept and display company and component type options using Select inputs.
    - Integrated a Switch for component state management and added Cancel and Save buttons for improved user interaction.
    - Utilized useRouter for navigation, enhancing the form's functionality and user experience.

--- src/app/dashboard/figma/[id]/ui/FigmaForm/FigmaForm.tsx
+++ src/app/dashboard/figma/[id]/ui/FigmaForm/FigmaForm.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { FigmaLinks } from "@/interfaces/design-system-interface";
-import { Input } from "@heroui/react";
+import {
+  Company,
+  ComponentType,
+  FigmaLinks,
+} from "@/interfaces/design-system-interface";
+import { Button, Input, Select, SelectItem, Switch } from "@heroui/react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 export interface FigmaFormValues {
@@ -14,9 +19,11 @@ export interface FigmaFormValues {
 
 interface FigmaFormProps {
   figma: FigmaLinks;
+  companies: Company[];
+  componentTypes: ComponentType[];
 }
 
-const FigmaForm = ({ figma }: FigmaFormProps) => {
+const FigmaForm = ({ figma, companies, componentTypes }: FigmaFormProps) => {
   const { register, handleSubmit } = useForm<FigmaFormValues>({
     defaultValues: {
       url: figma?.url,
@@ -25,13 +32,51 @@ const FigmaForm = ({ figma }: FigmaFormProps) => {
       componentTypeId: figma?.componentType?.id,
     },
   });
+  const companiesOptions = companies.map((company) => ({
+    label: company.name,
+    value: company.id,
+  }));
+  const componentTypesOptions = componentTypes.map((componentType) => ({
+    label: componentType.name,
+    value: componentType.id,
+  }));
+  const router = useRouter();
+
   const onSubmit = async (data: FigmaFormValues) => {
     console.log(data);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Input label="URL de Figma" {...register("url")} />
+      <div className="flex flex-col gap-4 pb-4">
+        <Input label="URL de Figma" {...register("url")} />
+        <Select label="Company" {...register("companyId")}>
+          {companiesOptions.map((company) => (
+            <SelectItem key={company.value} value={company.value}>
+              {company.label}
+            </SelectItem>
+          ))}
+        </Select>
+        <Select label="Tipo de componente" {...register("componentTypeId")}>
+          {componentTypesOptions.map((componentType) => (
+            <SelectItem key={componentType.value} value={componentType.value}>
+              {componentType.label}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+      <div className="flex justify-between items-center border-t border-gray-200 pt-4">
+        <Switch {...register("state")}>Estado del componente</Switch>
+
+        <div className="flex">
+          <Button variant="light" onPress={() => router.back()}>
+            Cancelar
+          </Button>
+          <Button type="submit" color="primary">
+            Guardar
+          </Button>
+        </div>
+      </div>
     </form>
   );
 };
