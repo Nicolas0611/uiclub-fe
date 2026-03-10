617257413bc576733145a5e87344c329d3b85110
Nicolás Sierra <nico.sierra13@gmail.com>
2026-03-09 22:55:52 -0500

    feat: Enhance ComponentTypePage to fetch and display component types
    - Updated ComponentTypePage to asynchronously fetch the list of component types using fetchComponentList.
    - Added a button for creating new component types, improving user interaction.
    - Structured the layout to include a header for the component table.

--- src/app/dashboard/component-type/page.tsx
+++ src/app/dashboard/component-type/page.tsx
@@ -1,5 +1,27 @@
-const ComponentTypePage = () => {
-  return <div>ComponentTypePage</div>;
+import { fetchComponentList } from "@/actions/component/component-action";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import { Button, Link } from "@heroui/react";
+
+const ComponentTypePage = async () => {
+  const componentTypes = await fetchComponentList();
+  console.log(componentTypes);
+  return (
+    <div className="flex flex-col h-full gap-3">
+      <div className="w-full flex items-center justify-end">
+        <Button
+          size="sm"
+          color="primary"
+          variant="solid"
+          as={Link}
+          href="/dashboard/component-type/new"
+        >
+          <PlusIcon className="size-5" />
+          Add Component Type
+        </Button>
+      </div>
+      <h3>Component Table</h3>
+    </div>
+  );
 };
 
 export default ComponentTypePage;
