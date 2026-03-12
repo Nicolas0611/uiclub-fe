8564db974d48fc753f304dea76258dd5cc81e168
Nicolás Sierra <nico.sierra13@gmail.com>
2026-03-11 23:15:07 -0500

    feat: Enhance ComponentTypeForm with design system integration and improved structure
    - Updated ComponentTypeForm to include design system and component type IDs in the form values.
    - Refactored component props to accept additional options for components and design systems.
    - Improved layout for image upload and selection fields, enhancing user experience.
    - Added TODO comment for future review of the refactor.

--- src/app/dashboard/component-type/[id]/ui/ComponentTypeForm/ComponentTypeForm.tsx
+++ src/app/dashboard/component-type/[id]/ui/ComponentTypeForm/ComponentTypeForm.tsx
@@ -2,7 +2,10 @@
 
 import Dropzone from "@/components/shared/Inputs/Drozpone/Dropzone";
 import { COMPONENT_TYPES } from "@/constants";
-import { ComponentType } from "@/interfaces/design-system-interface";
+import {
+  ComponentType,
+  DesignSystemComponentType,
+} from "@/interfaces/design-system-interface";
 import {
   Button,
   Checkbox,
@@ -26,12 +29,24 @@ export interface ComponentTypeFormValues {
   type: string;
   usageCount: number;
   componentImage: FileList;
+  designSystemId: string;
+  componentTypeId: string;
 }
+
+interface ComponentTypeFormProps {
+  componentType: ComponentType;
+  componentDesign: DesignSystemComponentType;
+  componentsOptions: { label: string; value: string }[]; //Todo: Review to make a global interface for options
+  designSystemsOptions: { label: string; value: string }[];
+}
+
+//TODO: REVIEW THIS REFACTOR https://claude.ai/chat/d87c5201-71ec-4e46-8662-87f7e7966aa9
 const ComponentTypeForm = ({
   componentType,
-}: {
-  componentType: ComponentType;
-}) => {
+  componentDesign,
+  componentsOptions,
+  designSystemsOptions,
+}: ComponentTypeFormProps) => {
   const { register, handleSubmit } = useForm<ComponentTypeFormValues>({
     defaultValues: {
       name: componentType.name,
@@ -42,6 +57,9 @@ const ComponentTypeForm = ({
       type: componentType.type,
       usageCount: componentType.usageCount,
       componentImage: undefined,
+
+      designSystemId: componentDesign?.designSystemId,
+      componentTypeId: componentDesign?.componentTypeId,
     },
   });
   const onSubmit = async (data: ComponentTypeFormValues) => {
@@ -51,6 +69,8 @@ const ComponentTypeForm = ({
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      {/* Component Type Form*/}
+
       <div className="flex flex-col gap-4 pb-4">
         <Input
           label="Nombre del componente"
@@ -80,29 +100,48 @@ const ComponentTypeForm = ({
         >
           Estado
         </Checkbox>
-        <Dropzone register={register} name="componentImage" />
-        {componentType?.componentImage && (
-          <div className="flex items-center flex-col gap-2">
-            <Image
-              src={componentType.componentImage.url}
-              alt={componentType.componentImage.url}
-              width={100}
-              height={100}
-            />
-            <Button
-              size="sm"
-              type="button"
-              variant="flat"
-              color="danger"
-              onPress={() => {
-                console.log("eliminar imagen");
-              }}
-            >
-              Eliminar
-            </Button>
-          </div>
-        )}
+        <div className="flex flex-row gap-2">
+          <Dropzone register={register} name="componentImage" />
+          {componentType?.componentImage && (
+            <div className="flex items-center flex-col gap-2">
+              <Image
+                src={componentType.componentImage.url}
+                alt={componentType.componentImage.url}
+                width={600}
+              />
+              <Button
+                size="sm"
+                type="button"
+                variant="flat"
+                color="danger"
+                onPress={() => {
+                  console.log("eliminar imagen");
+                }}
+              >
+                Eliminar
+              </Button>
+            </div>
+          )}
+        </div>
       </div>
+
+      {/* Related Components and Design Systems Form*/}
+
+      <div className="flex flex-col gap-4 border-t border-gray-200 py-4">
+        <Select label="Componente" {...register("componentTypeId")}>
+          {componentsOptions.map((component) => (
+            <SelectItem key={component.value}>{component.label}</SelectItem>
+          ))}
+        </Select>
+        <Select label="Design System" {...register("designSystemId")}>
+          {designSystemsOptions.map((designSystem) => (
+            <SelectItem key={designSystem.value}>
+              {designSystem.label}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+
       <div className="flex justify-end gap-4 border-t border-gray-200 pt-4">
         <Button type="submit" variant="light" onPress={() => router.back()}>
           Cancelar
