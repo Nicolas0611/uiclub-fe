99b984cb20b6e6b8f315b1998c0f08906275283f
Nicolás Sierra <nico.sierra13@gmail.com>
2026-03-17 21:38:23 -0500

    feat: Add create method to PrismaAdapter for enhanced model interactions
    - Implemented a new create method in the PrismaAdapter class to facilitate the creation of model instances.
    - This addition improves the adapter's functionality, allowing for more comprehensive database operations.

--- src/adapters/PrismaAdapter/PrismaAdapter.ts
+++ src/adapters/PrismaAdapter/PrismaAdapter.ts
@@ -34,4 +34,7 @@ export class PrismaAdapter<RES_T, OPT_T> implements HttpsReq<RES_T, OPT_T> {
   update(options?: OPT_T): Promise<RES_T> {
     return this.model.update(options);
   }
+  create(options?: OPT_T): Promise<RES_T> {
+    return this.model.create(options);
+  }
 }
